4e7cf1212ac6c1c3aa3d49b469094a62afdef591
Node A <58964683+CollegeSeniorUser@users.noreply.github.com>
2024-04-17 00:58:50 -0700

    change to numscore update3

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,259 @@
-import React, { useState, useEffect } from 'react';
-import { ref, query, onValue, runTransaction, orderByChild, equalTo, get, set } from 'firebase/database';
-import { AgGridReact } from '@ag-grid-community/react';
-import '@ag-grid-community/styles/ag-grid.css';
-import '@ag-grid-community/styles/ag-theme-quartz.css';
-import '@ag-grid-community/styles/ag-theme-alpine.min.css';
+import { useState, useEffect } from "react";
+import {
+  ref,
+  query,
+  onValue,
+  runTransaction,
+  orderByChild,
+  equalTo,
+} from "firebase/database";
+import { AgGridReact } from "@ag-grid-community/react";
+import "@ag-grid-community/styles/ag-grid.css";
+import "@ag-grid-community/styles/ag-theme-quartz.css";
+import "@ag-grid-community/styles/ag-theme-alpine.min.css";
 
-import { ModuleRegistry } from '@ag-grid-community/core';
-import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
-import { saveToLocalStorage, getFromLocalStorage } from './utils';
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+import { saveToLocalStorage, getFromLocalStorage } from "./utils";
 
-// Assuming these imports correctly point to your Firebase configuration
-import { database as db1 } from '../firebase.db1.config';
-import { database as db2 } from '../firebase.db2.config';
+// The configuration files for the two different databases
+import { database as db1 } from "../firebase.db1.config";
+import { database as db2 } from "../firebase.db2.config";
 
 ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
-const App = () => {
-  const [userData, setUserData] = useState(getFromLocalStorage('data') || {});
+const isDarkMode =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+function App() {
+  // Row Data: The data to be displayed.
+  const [userData, setUserData] = useState(getFromLocalStorage("data") || {});
   const [rowData, setRowData] = useState([]);
-  const [typeOfFood, setTypeOfFood] = useState('');
+  const [typeOfFood, setTypeOfFood] = useState();
 
-  useEffect(() => {
-    if (!typeOfFood) return;
+  // const myQuery = query(ref(database, "restaurants"));
 
-    const fetchRowData = async () => {
-      const fetchDataFromDb = async (db) => {
-        const dbQuery = query(ref(db, 'restaurants'), orderByChild('Type of food'), equalTo(typeOfFood));
-        const snapshot = await get(dbQuery);
-        return snapshot.exists() ? snapshot.val() : {};
-      };
-
-      const dataDb1 = await fetchDataFromDb(db1);
-      const dataDb2 = await fetchDataFromDb(db2);
-
-      // Combine and transform data from both databases
-      const combinedData = [...Object.keys(dataDb1).map(key => ({ ...dataDb1[key], id: key, db: 'db1' })),
-                             ...Object.keys(dataDb2).map(key => ({ ...dataDb2[key], id: key, db: 'db2' }))];
-
-      setRowData(combinedData);
-    };
-
-    fetchRowData();
-  }, [typeOfFood]);
-
-  const onEdit = (rowData, property) => async (e) => {
-    const db = rowData.db === 'db1' ? db1 : db2;
-    const restaurantRef = ref(db, `restaurants/${rowData.id}`);
-    const updateValue = e.target.checked ? 1 : -1;
-
-    // Transaction to update likes, dislikes, and 'Num of score'
-    runTransaction(restaurantRef, (currentData) => {
-      if (currentData) {
-        const scoreData = currentData.Score || {};
-        scoreData.Likes = scoreData.Likes || 0;
-        scoreData.Dislikes = scoreData.Dislikes || 0;
-
-        if (property === 'like') {
-          scoreData.Likes += updateValue;
-        } else if (property === 'dislike') {
-          scoreData.Dislikes += updateValue;
-        }
-
-        scoreData['Num of score'] = scoreData.Likes - scoreData.Dislikes;
-        currentData.Score = scoreData;
-
-        return currentData;
-      }
-
-      return;
-    });
-
-    // Update local storage and userData state
-    const newUserdata = { ...userData, [rowData.id]: { ...(userData[rowData.id] || {}), [property]: e.target.checked } };
-    saveToLocalStorage('data', newUserdata);
-    setUserData(newUserdata);
+  const handlePickTypeOfFood = (e) => {
+    setTypeOfFood(e.target.value);
   };
 
-  const colDefs = [
-    { field: 'Name', filter: true },
-    { field: 'Type of food', headerName: 'Type of Food', filter: true },
-    { field: 'Score.Likes', headerName: 'Total Likes' },
-    { field: 'Score.Dislikes', headerName: 'Total Dislikes' },
-    { field: 'Score["Num of score"]', headerName: 'Num of Score' },
-    // Add other columns as necessary
-  ];
+  // Whenever we detect a change in the type of food, we query both databases
+  // Then we "tranform" the data to a format that the grid can understand
+  // Before displaying it
+  useEffect(() => {
+    if (!typeOfFood) return;
+    const dbOneQuery = query(
+      ref(db1, "restaurants"),
+      orderByChild("Type of food"),
+      equalTo(typeOfFood)
+    );
 
+    const dbTwoQuery = query(
+      ref(db2, "restaurants"),
+      orderByChild("Type of food"),
+      equalTo(typeOfFood)
+    );
+
+    setRowData([]);
+    onValue(dbOneQuery, (snapshot) => {
+      const data = [];
+
+      snapshot.forEach((child) => {
+        data.push({ ...child.val(), id: child.key });
+      });
+
+      setRowData((prev) => [
+        ...prev,
+        // eslint-disable-next-line no-unused-vars
+        ...Object.entries(data).map(([id, value]) => ({
+          id: value.id,
+          ...value,
+          db: "db1",
+          userData: userData[value.id],
+        })),
+      ]);
+    });
+
+    onValue(dbTwoQuery, (snapshot) => {
+      const data = [];
+
+      snapshot.forEach((child) => {
+        data.push({ ...child.val(), id: child.key });
+      });
+
+      setRowData((prev) => [
+        ...prev,
+        // eslint-disable-next-line no-unused-vars
+        ...Object.entries(data).map(([id, value]) => ({
+          id: value.id,
+          ...value,
+          db: "db2",
+          userData: userData[value.id],
+        })),
+      ]);
+    });
+  }, [userData, typeOfFood]);
+
+  // This function is called whenever a user clicks on a checkbox
+  // It updates the data in the database and in the local storage
+  // It also updates the state of the userData (Which is really just a helper along with the local storage)
+  const onEdit = (rowData, property) => (e) => {
+    // Update realtime DB
+
+    const db = rowData.db === "db1" ? db1 : db2;
+    if (property === "like") {
+      const likesRef = ref(db, `restaurants/${rowData.id}/Score/Likes`);
+
+      runTransaction(likesRef, (currentLikes) => {
+        // if currentLikes has never been set, currentLikes will be `null`.
+        return (currentLikes || 0) + (e.target.checked ? 1 : -1);
+      });
+
+      console.log("id", "like-" + rowData.id);
+      if (e.target.checked && rowData.userData?.dislike) {
+        const likesRef = ref(db, `restaurants/${rowData.id}/Score/Dislikes`);
+
+        runTransaction(likesRef, (currentDislikes) => {
+          // if currentLikes has never been set, currentLikes will be `null`.
+          return (currentDislikes || 0) - 1;
+        });
+      }
+    }
+
+    if (property === "dislike") {
+      const likesRef = ref(db, `restaurants/${rowData.id}/Score/Dislikes`);
+
+      runTransaction(likesRef, (currentDisikes) => {
+        // if currentLikes has never been set, currentLikes will be `null`.
+        return (currentDisikes || 0) + (e.target.checked ? 1 : -1);
+      });
+
+      if (e.target.checked && rowData.userData?.like) {
+        const likesRef = ref(db, `restaurants/${rowData.id}/Score/Likes`);
+
+        runTransaction(likesRef, (currentLikes) => {
+          // if currentLikes has never been set, currentLikes will be `null`.
+          return (currentLikes || 0) - 1;
+        });
+      }
+    }
+
+    saveToLocalStorage("data", {
+      [rowData.id]: { [property]: e.target.checked },
+    });
+
+    setUserData((prev) => ({
+      ...prev,
+      [rowData.id]: { [property]: e.target.checked },
+    }));
+  };
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs] = useState([
+    { field: "Location", filter: true },
+    { field: "Name", filter: true },
+    { field: "Type of food", headerName: "Type of Food", filter: true },
+
+    { field: "Score.Likes", headerName: "Total number of likes" },
+    {
+      field: "like",
+      cellRenderer: (params) => (
+        <div className="checkbox-parent">
+          <label className="checkbox-container">
+            <input
+              id={"like-" + params.data.id}
+              onChange={onEdit(params.data, "like")}
+              className="custom-checkbox"
+              defaultChecked={params.data.userData?.like}
+              type="checkbox"
+            />
+            <span className="checkmark" />
+          </label>
+        </div>
+      ),
+    },
+
+    { field: "Score.Dislikes", headerName: "Total number of dislikes" },
+    {
+      field: "dislike",
+      cellRenderer: (params) => (
+        <div className="checkbox-parent">
+          <label className="checkbox-container">
+            <input
+              id={"dislike-" + params.data.id}
+              onChange={onEdit(params.data, "dislike")}
+              className="custom-checkbox red"
+              defaultChecked={params.data.userData?.dislike}
+              type="checkbox"
+            />
+            <span className="checkmark" />
+          </label>
+        </div>
+      ),
+    },
+    {
+      field: "Rating",
+      cellRenderer: ({ data }) => {
+        const totalVotes = data.Score.Likes + data.Score.Dislikes;
+
+        if (totalVotes === 0) {
+          return 0; // Default value when there are no votes
+        }
+
+        const rating = (data.Score.Likes / totalVotes) * 5;
+        return <b>{rating.toFixed(2)}</b>;
+      },
+    },
+    { field: "Score.Likes", headerName: "Total number of likes" },
+
+    {
+      field: "Num of score",
+      cellRenderer: ({ data }) => {
+        const totalVotes = data.Score.Likes + data.Score.Dislikes;
+        return <b>{totalVotes}</b>;
+      },
+    },
+
+  ]);
+
+  // Container: Defines the grid's theme & dimensions.
   return (
-    <div className={window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'ag-theme-alpine-dark' : 'ag-theme-alpine'} style={{ height: 600, width: '100%' }}>
-      <select onChange={(e) => setTypeOfFood(e.target.value)} value={typeOfFood}>
-        <option value="">Select Type of Food</option>
-        <option value="japanese">Japanese</option>
-        <option value="italian">Italian</option>
-        <option value="mexican">Mexican</option>
-        // Add other options as necessary
+    <div
+      className={isDarkMode ? "ag-theme-quartz-dark" : "ag-theme-quartz"}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <select
+        name="typeoffood"
+        id="typeoffood"
+        onChange={handlePickTypeOfFood}
+        value={typeOfFood}>
+        <option value=""></option>
+        <option value="british">British</option>
+        <option value="french">French</option>
+        <option value="korean">Korean</option>
+        <option value="spanish">Spanish</option>
+        <option value="other">Other</option>
       </select>
+ 
+
       <AgGridReact
-        rowData={rowData}
+        rowData={
+          // Sorting the data by Name
+          rowData.sort((a, b) => {
+            if (a.Name < b.Name) {
+              return -1;
+            }
+            if (a.Name > b.Name) {
+              return 1;
+            }
+            return 0;
+          })
+        }
         columnDefs={colDefs}
-        domLayout='autoHeight'
       />
     </div>
   );
-};
+}
 
 export default App;
